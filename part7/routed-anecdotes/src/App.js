4c7c39cd3528f8f21c0e44b9b5c896a830994cc1
Alireza <alireza.gholami403@gmail.com>
2021-11-25 18:28:28 +0330

    feat: add Exercise 7.6.

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -92,15 +92,27 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input {...content} />
+          <input
+            value={content.value}
+            type={content.type}
+            onChange={content.onChange}
+          />
         </div>
         <div>
           author
-          <input {...author} />
+          <input
+            value={author.value}
+            type={author.type}
+            onChange={author.onChange}
+          />
         </div>
         <div>
           url for more info
-          <input {...info} />
+          <input
+            value={info.value}
+            type={info.type}
+            onChange={info.onChange}
+          />
         </div>
         <button>create</button>
         <button onClick={reset} >reset</button>

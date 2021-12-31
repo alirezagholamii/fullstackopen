184bba7ccc08614fc9024ea195df77dba47d4a86
Alireza <alireza.gholami403@gmail.com>
2021-12-31 18:13:31 +0330

    feat: add Exercise 8.7.

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -106,7 +106,12 @@ const typeDefs = gql`
       author: String!,
       published: Int!,
       genres: [String]!
-    ): Book   
+    ): Book
+
+    editAuthor(
+      name: String!
+      born: Int!
+    ): Author
   } 
 `
 
@@ -154,6 +159,15 @@ const resolvers = {
         })
       }
       return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((item) => item.name === args.name)
+      if (!author) {
+        return null
+      }
+      const updatedAuthor = { ...author, born: args.born }
+      authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+      return updatedAuthor
     }
   }
 
